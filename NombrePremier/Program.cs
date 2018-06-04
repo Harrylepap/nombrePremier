382315fdd122f9cfe6af891caf65f8e754589836
Harrylepap <rakotondratompobakoharry@yahoo.fr>
2018-06-04 20:15:55 +0300

    Premier HelloWorld

--- NombrePremier/Program.cs
+++ NombrePremier/Program.cs
@@ -10,6 +10,8 @@ namespace NombrePremier
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
         }
     }
 }
